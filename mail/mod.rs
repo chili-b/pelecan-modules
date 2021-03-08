6be4d5c80e123edb3e022ab21689fadf31fe98c4
chili-b <dexter.gaonshatford@gmail.com>
2021-03-08 16:15:58 -0500

    update mail

--- mail/mod.rs
+++ mail/mod.rs
@@ -8,63 +8,65 @@ use std::sync::{Arc, Mutex};
 
 const MAX_MESSAGES: usize = 5;
 
-pub fn chat_filter<T>(_t: Arc<Mutex<T>>, mut c: V1Client, filter: mut TextMessage_Filter) -> FutureValue<(bool, TextMessage_Filter)> {
-    if filter.message.is_none() || filter.server.is_none() {
-        return future(true);
-    }
-    let text = filter.message.as_ref().unwrap().text().to_owned();
-    if filter.message.as_ref().unwrap().actor.is_none() {
-        return future(true);
-    }
-    let mut user = filter.message.as_ref().unwrap().actor.as_ref().unwrap().to_owned();
-    if text.starts_with("!mail") {
-        filter.set_action(Action::Drop);
-        let filter = filter.to_owned();
-        return future_from_async(async move {
-            // for whatever reason, user id is not available to the filter stream, so we have to do
-            // a database query... very stupid, but that's the way it is.
-            user = if let Ok(user) = c.user_get(user).await {
-                user.into_inner()
-            } else {
-                return false;
-            };
-            if user.id.is_none() {
-                return true;
-            }
-            let words: Vec<&str> = text.split(" ").collect();
-            if words.len() < 2 {
-                let server = filter.server.clone();
-                drop(c.text_message_send(TextMessage {
-                    server: server,
-                    users: vec![user],
-                    text: Some("<br/><h3>📨 !mail commands:</h3><ul> \
-                               <li><tt>!mail send <b>recipient</b> <b>message</b></tt><br/>(Send a <b>message</b> to the user with the given name.)</li> \
+pub fn chat_filter<T>(t: Arc<Mutex<Data>>, mut c: V1Client, filter: mut TextMessage_Filter) -> FutureValue<(bool, TextMessage_Filter)> {
+    future(async move {
+        if filter.message.is_none() || filter.server.is_none() {
+            return (true, filter);
+        }
+        let text = filter.message.as_ref().unwrap().text().to_owned();
+        if filter.message.as_ref().unwrap().actor.is_none() {
+            return (true, filter);
+        }
+        let mut user = filter.message.as_ref().unwrap().actor.as_ref().unwrap().to_owned();
+        if text.starts_with("!mail") {
+            filter.set_action(Action::Drop);
+            let filter = filter.to_owned();
+            return future_from_async(async move {
+                // for whatever reason, user id is not available to the filter stream, so we have to do
+                // a database query... very stupid, but that's the way it is.
+                user = if let Ok(user) = c.user_get(user).await {
+                    user.into_inner()
+                } else {
+                    return false;
+                };
+                if user.id.is_none() {
+                    return true;
+                }
+                let words: Vec<&str> = text.split(" ").collect();
+                if words.len() < 2 {
+                    let server = filter.server.clone();
+                    drop(c.text_message_send(TextMessage {
+                        server: server,
+                        users: vec![user],
+                        text: Some("<br/><h3>📨 !mail commands:</h3><ul> \
+                                   <li><tt>!mail send <b>recipient</b> <b>message</b></tt><br/>(Send a <b>message</b> to the user with the given name.)</li> \
                                 <li><tt>!mail read <b>number</b></tt><br/>(Read a message. Typing <tt>!mail read</tt> without a number will open your mailbox.)</li> \
                                 <li><tt>!mail delete <b>number</b></tt><br/>(Delete a message. Typing <tt>!mail delete</tt> without a number will empty your mailbox.)</li></ul>".to_string()),
                                 channels: vec![], trees: vec![], actor: None
-                }).await);
-                false
-            } else {
-                match words[1] {
-                    "send" => send_message(c, t, filter.server.to_owned(), user, text[10..].to_owned()).await,
-                    "read" => read_message(c, t, filter.server.to_owned(), user, text[10..].to_owned()).await,
-                    "delete" => delete_message(c, t, filter.server.to_owned(), user, text[12..].to_owned()).await,
-                    _ => false
+                    }).await);
+                    false
+                } else {
+                    match words[1] {
+                        "send" => send_message(c, t, filter.server.to_owned(), user, text[10..].to_owned()).await,
+                        "read" => read_message(c, t, filter.server.to_owned(), user, text[10..].to_owned()).await,
+                        "delete" => delete_message(c, t, filter.server.to_owned(), user, text[12..].to_owned()).await,
+                        _ => false
+                    }
                 }
-            }
-        });
-    }
-    future_from_bool(true)
+            });
+        }
+        true
+    })
 }
 
-pub fn user_connected(t: Arc<Mutex<Data>>, mut c: Client, e: &Server_Event) -> FutureValue<bool> {
-    let e = e.to_owned();
-    if e.user.is_none() || e.user.as_ref().unwrap().id.is_none() {
-        return future_from_bool(true);
-    }
-    let user = e.user.unwrap();
-    let server = e.server.clone();
-    future_from_async(async move { 
+pub fn user_connected(t: Arc<Mutex<Data>>, mut c: Client, e: Server_Event) -> FutureValue<bool> {
+    future(async move {
+        let e = e.to_owned();
+        if e.user.is_none() || e.user.as_ref().unwrap().id.is_none() {
+            return true;
+        }
+        let user = e.user.unwrap();
+        let server = e.server.clone();
         let server_path = &t.lock_async().await.path;
         let mailbox = load_mailbox(user.id(), server_path);
         if mailbox.messages.iter().any(|msg| msg.0) { // if at least one message is unread
@@ -79,9 +81,9 @@ pub fn user_connected(t: Arc<Mutex<Data>>, mut c: Client, e: &Server_Event) -> F
     })
 }
 
-fn delete_message(mut c: Client, mut t: Arc<Mutex<Data>>, server: Option<Server>, user: User, message: String) -> FutureValue<bool> {
-    let message = message.trim().to_owned();
-    future_from_async(async move {
+fn delete_message(mut c: V1Client, mut t: Arc<Mutex<Data>>, server: Option<Server>, user: User, message: String) -> FutureValue<bool> {
+    future(async move {
+        let message = message.trim().to_owned();
         let server_path = &t.lock_async().await.path;
         let mut mailbox = load_mailbox(user.id(), server_path);
         if message.len() == 0 {
@@ -116,13 +118,13 @@ fn delete_message(mut c: Client, mut t: Arc<Mutex<Data>>, server: Option<Server>
     })
 }
 
-fn send_message(mut c: Client, mut t: DataMutex<Data>, server: Option<Server>, user: User, message: String) -> FutureValue<bool> {
-    let message = message.trim_start();
-    if let Some(index) = message.find(" ") {
-        let (recipient, message) = message.trim_start().split_at(index);
-        let recipient = recipient.to_owned();
-        let message = message.trim().to_owned();
-        return future_from_async(async move {
+fn send_message(mut c: V1Client, mut t: Arc<Mutex<Data>>, server: Option<Server>, user: User, message: String) -> FutureValue<bool> {
+    future(async move {
+        let message = message.trim_start();
+        if let Some(index) = message.find(" ") {
+            let (recipient, message) = message.trim_start().split_at(index);
+            let recipient = recipient.to_owned();
+            let message = message.trim().to_owned();
             let server_path = t.lock_async().await.path.to_owned();
             if let Some(recipient_id) = user_id_from_name(c.clone(), server.clone(), &recipient).await {
                 let mut mailbox = load_mailbox(recipient_id, &server_path);
@@ -135,7 +137,7 @@ fn send_message(mut c: Client, mut t: DataMutex<Data>, server: Option<Server>, u
                         text: Some(format!(
                                 "Message sent to <b><span style=\"color:#00aa7f\">{}</span></b>'s mailbox", 
                                 recipient)),
-                        channels: vec![], trees: vec![], actor: None
+                                channels: vec![], trees: vec![], actor: None
                     };
                     drop(store_mailbox(&mailbox, &server_path));
                     drop(c.text_message_send(success_message).await);
@@ -146,7 +148,7 @@ fn send_message(mut c: Client, mut t: DataMutex<Data>, server: Option<Server>, u
                         text: Some(format!(
                                 "<b><span style=\"color:#00aa7f\">{}</span></b>'s mailbox is full so your message was not delivered", 
                                 recipient)),
-                        channels: vec![], trees: vec![], actor: None
+                                channels: vec![], trees: vec![], actor: None
                     };
                     drop(c.text_message_send(error_message).await);
                 }
@@ -157,19 +159,19 @@ fn send_message(mut c: Client, mut t: DataMutex<Data>, server: Option<Server>, u
                     text: Some(format!(
                             "<b><span style=\"color:#aa0000\">Couldn't find a user with the name `{}`</span></b>",
                             recipient)),
-                    channels: vec![], trees: vec![], actor: None
+                            channels: vec![], trees: vec![], actor: None
                 };
                 drop(c.text_message_send(error_message).await);
             }
             false
-        });
-    }
-    future_from_bool(false)
+        }
+        false
+    })
 }
 
-fn read_message(mut c: Client, mut t: DataMutex<Data>, server: Option<Server>, user: User, message: String) -> FutureBool {
-    let message = message.trim().to_owned();
-    future_from_async(async move {
+fn read_message(mut c: V1Client, mut t: Arc<Mutex<Data>>, server: Option<Server>, user: User, message: String) -> FutureValue<bool> {
+    future(async move {
+        let message = message.trim().to_owned();
         let server_path = &t.lock_async().await.path;
         let mut mailbox = load_mailbox(user.id(), server_path);
         mailbox.owner = user.id(); // if the mailbox is newly created, it will have the wrong id.
@@ -197,7 +199,7 @@ fn read_message(mut c: Client, mut t: DataMutex<Data>, server: Option<Server>, u
     })
 }
 
-async fn user_id_from_name(mut c: Client, server: Option<Server>, name: &str) -> Option<u32> {
+async fn user_id_from_name(mut c: V1Client, server: Option<Server>, name: &str) -> Option<u32> {
     let query = Query {server: server, filter: Some(name.to_string())};
     if let Ok(response) = c.database_user_query(query).await {
         let list = response.into_inner();
@@ -208,7 +210,7 @@ async fn user_id_from_name(mut c: Client, server: Option<Server>, name: &str) ->
     None
 }
 
-async fn user_name_from_id(mut c: Client, server: Option<Server>, id: u32) -> Option<String> {
+async fn user_name_from_id(mut c: V1Client, server: Option<Server>, id: u32) -> Option<String> {
     let database_user = DatabaseUser {
         server: server, id: Some(id), name: None,
         email: None, comment: None, hash: None,
@@ -220,7 +222,7 @@ async fn user_name_from_id(mut c: Client, server: Option<Server>, id: u32) -> Op
     None
 }
 
-async fn message_string(c: Client, server: Option<Server>, message: Message) -> Option<String> {
+async fn message_string(c: V1Client, server: Option<Server>, message: TextMessage) -> Option<String> {
     if let Some(name) = user_name_from_id(c, server, message.sender).await {
         return Some(format!("<h3>✉️ From: {}</h3>{}", name, message.contents));
     }
